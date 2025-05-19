7484ba9075143926733421f823b30bbe3a9b9b7c
ryumin <kys15487@gmail.com>
2025-05-19 10:11:18 +1000

    Update google-n8n-credential.js

--- google-n8n-credential.js
+++ google-n8n-credential.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  'https://your-redirect-url.com/auth/google/callback' // 리디렉션 URL
+  process.env.GOOGLE_REDIRECT_URI
 );
 
 const generateCredentialForN8n = async (token) => {
